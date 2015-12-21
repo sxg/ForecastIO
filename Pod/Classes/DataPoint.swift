66ca453d319b107a910b6676031b0f7e11167a8d
Satyam Ghodasara <sghodas@gmail.com>
2015-12-20 22:53:18 -0500

    Added docs for DataPoint.swift

--- Pod/Classes/DataPoint.swift
+++ Pod/Classes/DataPoint.swift
@@ -8,40 +8,112 @@
 
 import Foundation
 
+/// Weather data for a specific location and time.
 public struct DataPoint {
+
+    /// The time at which this DataPoint occurs.
     public let time: NSDate
+    
+    /// A human-readable text summary of the weather.
     public let summary: String?
+    
+    /// A machine-readable summary of the weather suitable for selecting an icon for display.
     public let icon: Icon?
+    
+    /// The time of the last sunrise before the solar noon closest to local noon on the given day. Only defined on Forecast's `daily` DataPoints. Note: near the poles, this may occur on a different day entirely!
     public let sunriseTime: NSDate?
+    
+    /// The time of the first sunset after the solar noon closest to local noon on the given day. Only defined on Forecast's `daily` DataPoints. Note: near the poles, this may occur on a different day entirely!
     public let sunsetTime: NSDate?
+    
+    /// The fractional part of the lunation number of the given day. This can be thought of as the "percentage complete" of the current lunar month. A value of `0` represents a new moon, a value of `0.25` represents a first quarter moon, a value of `0.5` represents a full moon, and a value of `0.75` represents a last quarter moon. The ranges between these values represent waxing crescent, waxing gibbous, waning gibbous, and waning crescent moons, respectively. Only defined on Forecast's `daily` DataPoints.
     public let moonPhase: Float?
+    
+    /// The distance to the nearest storm in miles. This value is *very approximate* and should not be used in scenarios requiring accurate results. A storm distance of `0` doesn't necessarily refer to a storm at the requested location, but rather a storm in the vicinity of the requested location. Only defined on Forecast's `currently` DataPoints.
     public let nearestStormDistance: Float?
+    
+    /// The direction of the nearest storm in degrees, with true north at 0º and progressing clockwise. If `nearestStormDistance` is `0`, then this value will be `nil`. The caveats that apply to `nearestStormDistance` apply to this too. Only defined on Forecast's `currently` DataPoints.
     public let nearestStormBearing: Float?
+    
+    /// The average expected intensity in inches of liquid water per hour of precipitation occurring at the given time *conditional on probability* (assuming any precipitation occurs at all). A *very* rough guide is that a value of `0` corresponds to no precipitation, `0.002` corresponds to very light precipitation, `0.017` corresponds to light precipitation, `0.1` corresponds to moderate precipitation, and `0.4` corresponds to heavy precipitation.
     public let precipIntensity: Float?
+    
+    /// Maximum expected intensity of precipitation on the given day in inches of liquid water per hour. Only defined on Forecast's `daily` DataPoints.
     public let precipIntensityMax: Float?
+    
+    /// Time at which the maximum expected intensity of precipitation will occur. Only defined on Forecast's `daily` DataPoints.
     public let precipIntensityMaxTime: NSDate?
+    
+    /// Value between `0` and `1` (inclusive) representing the probability of precipitation occurring at the given time.
     public let precipProbability: Float?
+    
+    ///
     public let precipType: String?
+    
+    /// The amount of snowfall accumulation expected to occur on the given day, in inches. This will be `nil` if no accumulation is expected. Only defined on Forecast's `hourly` and `daily` DataPoints.
     public let precipAccumulation: Float?
-    public let temperature: Float?                    //  Fahrenheit
-    public let temperatureMin: Float?                 //  Fahrenheit
+    
+    /// The temperature at the given time in degrees Fahrenheit. Not defined on Forecast's `daily` DataPoints.
+    public let temperature: Float?
+    
+    /// The minimum temperature on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
+    public let temperatureMin: Float?
+    
+    /// The time at which the minimum temperature will occur on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
     public let temperatureMinTime: NSDate?
-    public let temperatureMax: Float?                 //  Fahrenheit
+    
+    /// The maximum temperature on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
+    public let temperatureMax: Float?
+    
+    /// The time at which the maximum temperature will occur on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
     public let temperatureMaxTime: NSDate?
-    public let apparentTemperature: Float?            //  Fahrenheit
-    public let apparentTemperatureMin: Float?         //  Fahrenheit
+    
+    /// The apparent or "feels like" temperature at the given time in degrees Fahrenheit. Not defined on Forecast's `daily` DataPoints.
+    public let apparentTemperature: Float?
+    
+    /// The minimum apparent or "feels like" temperature on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
+    public let apparentTemperatureMin: Float?
+    
+    /// The time at which the minimum apparent or "feels like" temperature will occur on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
     public let apparentTemperatureMinTime: NSDate?
-    public let apparentTemperatureMax: Float?         //  Fahrenheit
+    
+    /// The maximum apparent or "feels like" temperature on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
+    public let apparentTemperatureMax: Float?
+    
+    /// The time at which the maximum apparent or "feels like" temperature will occur on the given day in degrees Fahrenheit. Only defined on Forecast's `daily` DataPoints.
     public let apparentTemperatureMaxTime: NSDate?
+    
+    /// The dew point at the given time in degrees Fahrenheit.
     public let dewPoint: Float?
+    
+    /// The wind speed at the given time in miles per hour.
     public let windSpeed: Float?
+    
+    /// The direction that the wind is coming from in degrees, with true north at 0º and progressing clockwise. If `windSpeed` is `0`, then this will be `nil`.
     public let windBearing: Float?
+    
+    /// Value between `0` and `1` (inclusive) representing the percentage of sky occluded by clouds. A value of `0` corresponds to a clear sky, `0.4` corresponds to scattered clouds, `0.75` correspond to broken cloud cover, and `1` corresponds to completely overcast skies.
     public let cloudCover: Float?
+    
+    /// Value between `0` and `1` (inclusive) representing the relative humidity.
     public let humidity: Float?
+    
+    /// The sea-level air pressure in millibars.
     public let pressure: Float?
+    
+    /// The average visibility in miles, capped at `10`.
     public let visibility: Float?
+    
+    /// The columnar density of total atomspheric ozone at the given time in Dobson units.
     public let ozone: Float?
     
+    /**
+        Creates a new DataPoint from a JSON object.
+     
+        - Parameter fromJSON: A JSON object with keys corresponding to the DataPoint's properties.
+     
+        - Returns: A new DataPoint filled with data from the given JSON object.
+     */
     init(fromJSON json: NSDictionary) {
         time = NSDate(timeIntervalSince1970: json["time"] as! Double)
         summary = json["summary"] as? String
