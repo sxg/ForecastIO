329bcbe0b38dea60a4ceaff51a3cb8eef715739f
Satyam Ghodasara <sghodas@gmail.com>
2015-12-20 21:48:48 -0500

    Added docs for Alert.swift

--- Pod/Classes/Alert.swift
+++ Pod/Classes/Alert.swift
@@ -8,12 +8,28 @@
 
 import Foundation
 
+/// A severe weather warning issued for a location by a governmental authority (consult Forecast.io's Dark Sky API documentation for a full list).
 public struct Alert {
+    
+    /// A short text summary of the Alert.
     public let title: String
+    
+    /// The time at which the Alert will cease to be valid.
     public let expires: NSDate
+    
+    /// A detailed text description of the Alert from the appropriate weather service.
     public let description: String?
+    
+    /// An HTTP(S) URI that contains detailed information about the Alert.
     public let uri: NSURL
     
+    /**
+        Creates an Alert from a JSON object.
+     
+        - Parameter fromJSON: A JSON object with keys corresponding to the Alert's properties.
+     
+        - Returns: A new Alert filled with data from the given JSON object.
+    */
     init(fromJSON json: NSDictionary) {
         title = json["title"] as! String
         expires = NSDate(timeIntervalSince1970: json["expires"] as! Double)
