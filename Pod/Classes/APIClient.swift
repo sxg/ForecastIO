3fe81b1ac8b9799e29b2ff8d946bc7fbf51f84f6
Satyam Ghodasara <sghodas@gmail.com>
2015-12-20 21:31:24 -0500

    Added docs for APIClient.swift

--- Pod/Classes/APIClient.swift
+++ Pod/Classes/APIClient.swift
@@ -14,15 +14,37 @@ public class APIClient : NSObject {
     private let session = NSURLSession.sharedSession()
     private static let forecastIOURL = "https://api.forecast.io/forecast/"
     
+    /**
+        Creates a new APIClient to interact with Forecast.io's Dark Sky API.
+     
+        - Parameter apiKey: Your Dark Sky API key.
+     
+        - Returns: A new APIClient.
+    */
     public init(apiKey key: String) {
         apiKey = key
     }
     
+    /**
+        Gets the current forecast at a specified latitude and longitude and returns it in a block.
+     
+        - Parameter latitude: Latitude at which to get the forecast.
+        - Parameter longitude: Longitude at which to get the forecast.
+        - Parameter completion: A block that returns the forecast at the latitude and longitude you specified or an error.
+    */
     public func getForecast(latitude lat: Double, longitude lon: Double, completion: (forecast: Forecast?, error: NSError?) -> Void) {
         let url = NSURL(string: APIClient.forecastIOURL + apiKey + "/\(lat),\(lon)")!
         getForecast(url, completion: completion)
     }
     
+    /**
+        Gets the forecast at a specified latitude, longitude, and time, and returns it in a block.
+     
+        - Parameter latitude: Latitude at which to get the forecast.
+        - Parameter longitude: Longitude at which to get the forecast.
+        - Parameter time: Time at which to get the forecast. If no timezone is specified, local time (at the specified latitude and longitude) will be assumed.
+        - Parameter completion: A block that returns the forecast at the latitude and longitude you specified or an error.
+    */
     public func getForecast(latitude lat: Double, longitude lon: Double, time: NSDate, completion: (forecast: Forecast?, error: NSError?) -> Void) {
         let timeString = String(format: "%.0f", time.timeIntervalSince1970)
         let url = NSURL(string: APIClient.forecastIOURL + apiKey + "/\(lat),\(lon),\(timeString)")!
