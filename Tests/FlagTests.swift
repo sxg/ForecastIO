7f451e97cfd2a8208e0793ab79e3cef1291e4633
Satyam Ghodasara <sghodas@gmail.com>
2016-01-23 18:14:17 -0500

    Added Flag tests

---
+++ Tests/FlagTests.swift
@@ -0,0 +1,39 @@
+//
+//  FlagTests.swift
+//  ForecastIO
+//
+//  Created by Satyam Ghodasara on 1/23/16.
+//
+//
+
+import XCTest
+import Nimble
+import ForecastIO
+
+class FlagTests: XCTestCase {
+    
+    var flagJSON: NSDictionary!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let forecastJSONPath = NSBundle(forClass: self.dynamicType).pathForResource("forecast", ofType: "json")!
+        let forecastJSONData = NSData(contentsOfFile: forecastJSONPath)!
+        let forecastJSON = try! NSJSONSerialization.JSONObjectWithData(forecastJSONData, options: .MutableContainers) as! NSDictionary
+        self.flagJSON = forecastJSON["flags"] as! NSDictionary
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testInitFromJSON() {
+        //  Given
+        //  When
+        let flag = Flag(fromJSON: flagJSON)
+        
+        //  Then
+        expect(flag).toNot(beNil())
+    }
+    
+}
