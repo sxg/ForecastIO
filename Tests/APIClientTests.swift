eca5be09c663163c4e7d2fcb09fb45299cb0b1b2
Satyam Ghodasara <sghodas@gmail.com>
2016-01-23 18:37:01 -0500

    Added Units test

---
+++ Tests/APIClientTests.swift
@@ -0,0 +1,9 @@
+//
+//  APIClientTests.swift
+//  ForecastIO
+//
+//  Created by Satyam Ghodasara on 1/23/16.
+//
+//
+
+import Foundation
---
+++ Tests/UnitsTest.swift
@@ -0,0 +1,27 @@
+//
+//  UnitsTest.swift
+//  ForecastIO
+//
+//  Created by Satyam Ghodasara on 1/23/16.
+//
+//
+
+import XCTest
+import Nimble
+import ForecastIO
+
+class UnitsTests: XCTestCase {
+    
+    func testDescription() {
+        //  Given
+        //  When
+        //  Then
+        expect(Units.SI.description).to(equal("si"))
+        expect(Units.US.description).to(equal("us"))
+        expect(Units.CA.description).to(equal("ca"))
+        expect(Units.UK.description).to(equal("uk"))
+        expect(Units.UK2.description).to(equal("uk2"))
+        expect(Units.Auto.description).to(equal("auto"))
+    }
+    
+}
