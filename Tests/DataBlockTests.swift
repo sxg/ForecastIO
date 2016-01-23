adb9607af74abc91a9043125e2b07770f9e56f44
Satyam Ghodasara <sghodas@gmail.com>
2016-01-23 18:28:08 -0500

    Added DataBlock tests

---
+++ Tests/DataBlockTests.swift
@@ -0,0 +1,42 @@
+//
+//  DataBlockTests.swift
+//  ForecastIO
+//
+//  Created by Satyam Ghodasara on 1/23/16.
+//
+//
+
+import XCTest
+import Nimble
+import ForecastIO
+
+class DataBlockTests: XCTestCase {
+    
+    var dataBlockJSON: NSDictionary!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let forecastJSONPath = NSBundle(forClass: self.dynamicType).pathForResource("forecast", ofType: "json")!
+        let forecastJSONData = NSData(contentsOfFile: forecastJSONPath)!
+        let forecastJSON = try! NSJSONSerialization.JSONObjectWithData(forecastJSONData, options: .MutableContainers) as! NSDictionary
+        self.dataBlockJSON = forecastJSON["minutely"] as! NSDictionary
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testInitFromJSON() {
+        //  Given
+        //  When
+        let dataBlock = DataBlock(fromJSON: self.dataBlockJSON)
+        
+        //  Then
+        expect(dataBlock).toNot(beNil())
+        expect(dataBlock.summary).to(equal("Snow for the hour."))
+        expect(dataBlock.icon).to(equal(Icon.Snow))
+        expect(dataBlock.data).toNot(beEmpty())
+    }
+    
+}
