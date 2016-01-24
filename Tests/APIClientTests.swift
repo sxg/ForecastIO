4480ef93a796940fff45971663cc10642201b1f8
Satyam Ghodasara <sghodas@gmail.com>
2016-01-24 00:58:38 -0500

    Added tests for both getForecast methods

--- Tests/APIClientTests.swift
+++ Tests/APIClientTests.swift
@@ -14,19 +14,55 @@ import OHHTTPStubs
 class APIClientTests: XCTestCase {
     
     override func setUp() {
-        stub(isHost("https://api.forecast.io/forecast/")) { _ in
+        super.setUp()
+        
+        stub(isHost("api.forecast.io")) { _ in
             let forecastJSONPath = OHPathForFile("forecast.json", self.dynamicType)
             return fixture(forecastJSONPath!, headers: ["Content-Type": "application/json"])
         }
     }
     
+    override func tearDown() {
+        OHHTTPStubs.removeAllStubs()
+        
+        super.tearDown()
+    }
+    
     func testInit() {
         //  Given
         //  When
-        let client = APIClient(apiKey: "FAKE API KEY")
+        let client = APIClient(apiKey: "FAKE-API-KEY")
         
         //  Then
         expect(client).toNot(beNil())
+    }
+    
+    func testGetForecast() {
+        //  Given
+        let client = APIClient(apiKey: "FAKE-API-KEY")
+        var forecast: Forecast?
+        
+        //  When
+        client.getForecast(latitude: 41.499320, longitude: -81.694361) { (currentForecast, error) -> Void in
+            forecast = currentForecast
+        }
+        
+        //  Then
+        expect(forecast).toEventuallyNot(beNil())
+    }
+    
+    func testGetForecastForTime() {
+        //  Given
+        let client = APIClient(apiKey: "FAKE-API-KEY")
+        var forecast: Forecast?
+        
+        //  When
+        client.getForecast(latitude: 41.499320, longitude: -81.694361, time: NSDate()) { (aForecast, error) -> Void in
+            forecast = aForecast
+        }
+        
+        //  Then
+        expect(forecast).toEventuallyNot(beNil())
     }
     
 }
