ab8b9b33593f94b591ce7d17b274748ebec859ce
Satyam Ghodasara <sghodas@gmail.com>
2016-06-18 00:25:53 -0400

    Updated example project

--- Example/iOS Example/ViewController.swift
+++ Example/iOS Example/ViewController.swift
@@ -17,9 +17,8 @@ class ViewController: UIViewController {
         
         let client = APIClient(apiKey: "Test Key")
         client.units = .Auto
-        client.getForecast(latitude: 30, longitude: 30, excludeBlocks: [WeatherBlocks.Alerts,.Currently,.Daily,.Flags,.Minutely]) { (forecast, error) in
+        client.getForecast(latitude: 30, longitude: 30, excludeBlocks: [.Alerts, .Currently, .Daily, .Flags, .Minutely]) { (forecast, error) in
             print(forecast?.hourly?.data)
-            
         }
     }
 
