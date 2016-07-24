700b1d43286de99d6fef8ed04308a9f7a61fca5e
Satyam Ghodasara <sghodas@gmail.com>
2016-07-24 11:39:03 -0400

    Added missing forward slash on Language enum docs

--- Source/Language.swift
+++ Source/Language.swift
@@ -10,91 +10,91 @@ import Foundation
 
 /// Languages in which data from the Dark Sky API may be returned.
 public enum Language: String, CustomStringConvertible {
-    //  The Arabic language.
+    ///  The Arabic language.
     case Arabic = "ar"
     
-    //  The Belarusian language.
+    ///  The Belarusian language.
     case Belarusian = "be"
     
-    //  The Bosnian language.
+    ///  The Bosnian language.
     case Bosnian = "bs"
     
-    //  The Czech language.
+    ///  The Czech language.
     case Czech = "cs"
     
-    //  The German language.
+    ///  The German language.
     case German = "de"
     
-    //  The Greek language.
+    ///  The Greek language.
     case Greek = "el"
     
-    //  The English language and the default option.
+    ///  The English language and the default option.
     case English = "en"
     
-    //  The Spanish language.
+    ///  The Spanish language.
     case Spanish = "es"
     
-    //  The French language.
+    ///  The French language.
     case French = "fr"
     
-    //  The Croatian language.
+    ///  The Croatian language.
     case Croatian = "hr"
     
-    //  The Hungarian language.
+    ///  The Hungarian language.
     case Hungarian = "hu"
     
-    //  The Indonesian language.
+    ///  The Indonesian language.
     case Indonesian = "id"
     
-    //  The Italian language.
+    ///  The Italian language.
     case Italian = "it"
     
-    //  The Icelandic language.
+    ///  The Icelandic language.
     case Icelandic = "is"
     
-    //  The Cornish language.
+    ///  The Cornish language.
     case Cornish = "kw"
     
-    //  The NorwegianBokmål language.
+    ///  The NorwegianBokmål language.
     case NorwegianBokmål = "nb"
     
-    //  The Dutch language.
+    ///  The Dutch language.
     case Dutch = "nl"
     
-    //  The Polish language.
+    ///  The Polish language.
     case Polish = "pl"
     
-    //  The Portuguese language.
+    ///  The Portuguese language.
     case Portuguese = "pt"
     
-    //  The Russian language.
+    ///  The Russian language.
     case Russian = "ru"
     
-    //  The Slovak language.
+    ///  The Slovak language.
     case Slovak = "sk"
     
-    //  The Serbian language.
+    ///  The Serbian language.
     case Serbian = "sr"
     
-    //  The Swedish language.
+    ///  The Swedish language.
     case Swedish = "sv"
     
-    //  The Tetum language.
+    ///  The Tetum language.
     case Tetum = "tet"
     
-    //  The Turkish language.
+    ///  The Turkish language.
     case Turkish = "tr"
     
-    //  The Ukrainian language.
+    ///  The Ukrainian language.
     case Ukrainian = "uk"
     
-    //  The Pig Latin language.
+    ///  The Pig Latin language.
     case PigLatin = "x-pig-latin"
     
-    //  The simplified Chinese language.
+    ///  The simplified Chinese language.
     case SimplifiedChinese = "zh"
     
-    //  The traditional Chinese language.
+    ///  The traditional Chinese language.
     case TraditionalChinese = "zh-tw"
     
     /**
