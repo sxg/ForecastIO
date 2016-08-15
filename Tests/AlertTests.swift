a653185f6202a50ee46cabb8df2d16ebb74f245a
Satyam Ghodasara <sghodas@gmail.com>
2016-08-14 21:53:33 -0400

    Migrated AlertTests to use XCTest

--- Tests/AlertTests.swift
+++ Tests/AlertTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-import Nimble
 @testable import ForecastIO
 
 class AlertTests: XCTestCase {
@@ -41,11 +40,11 @@ class AlertTests: XCTestCase {
         let alert = Alert(fromJSON: alertJSON)
         
         //  Then
-        expect(alert).toNot(beNil())
-        expect(alert.title).to(equal("High Wind Warning for Baltimore, MD"))
-        expect(alert.expires).to(equal(Date(timeIntervalSince1970: 1453593600)))
-        expect(alert.uri.absoluteString).to(equal("http://alerts.weather.gov/cap/wwacapget.php?x=MD1255E4FAA7AC.HighWindWarning.1255E5079700MD.LWXNPWLWX.b0aa96f59630770f05e80c219b873fd2"))
-        expect(alert.description).to(equal("...HIGH WIND WARNING REMAINS IN EFFECT UNTIL 7 PM EST THIS\nEVENING...\n* TIMING...THROUGH TODAY.\n* WINDS...NORTH 25 TO 35 MPH WITH GUSTS UP TO 55 MPH.\n* IMPACTS...DOWNED TREES...POWERLINES...AND MINOR STRUCTURAL\nDAMAGE. DRIVING HIGH PROFILE VEHICLES IN THESE CONDITIONS IS\nDANGEROUS.\n"))
+        XCTAssertNotNil(alert)
+        XCTAssertEqual(alert.title, "High Wind Warning for Baltiomre, MD")
+        XCTAssertEqual(alert.expires, Date(timeIntervalSince1970: 1453593600))
+        XCTAssertEqual(alert.uri.absoluteString, "http://alerts.weather.gov/cap/wwacapget.php?x=MD1255E4FAA7AC.HighWindWarning.1255E5079700MD.LWXNPWLWX.b0aa96f59630770f05e80c219b873fd2")
+        XCTAssertEqual(alert.description, "...HIGH WIND WARNING REMAINS IN EFFECT UNTIL 7 PM EST THIS\nEVENING...\n* TIMING...THROUGH TODAY.\n* WINDS...NORTH 25 TO 35 MPH WITH GUSTS UP TO 55 MPH.\n* IMPACTS...DOWNED TREES...POWERLINES...AND MINOR STRUCTURAL\nDAMAGE. DRIVING HIGH PROFILE VEHICLES IN THESE CONDITIONS IS\nDANGEROUS.\n")
     }
     
     func testInitNoOptionalsFromJSON() {
@@ -56,10 +55,10 @@ class AlertTests: XCTestCase {
         let alert = Alert(fromJSON: alertJSON)
         
         //  Then
-        expect(alert).toNot(beNil())
-        expect(alert.title).to(equal("High Wind Warning for Baltimore, MD"))
-        expect(alert.expires).to(equal(Date(timeIntervalSince1970: 1453593600)))
-        expect(alert.uri.absoluteString).to(equal("http://alerts.weather.gov/cap/wwacapget.php?x=MD1255E4FAA7AC.HighWindWarning.1255E5079700MD.LWXNPWLWX.b0aa96f59630770f05e80c219b873fd2"))
-        expect(alert.description).to(beNil())
+        XCTAssertNotNil(alert)
+        XCTAssertEqual(alert.title, "High Wind Warning for Baltimore, MD")
+        XCTAssertEqual(alert.expires, Date(timeIntervalSince1970: 1453593600))
+        XCTAssertEqual(alert.uri.absoluteString, "http://alerts.weather.gov/cap/wwacapget.php?x=MD1255E4FAA7AC.HighWindWarning.1255E5079700MD.LWXNPWLWX.b0aa96f59630770f05e80c219b873fd2")
+        XCTAssertNil(alert.description)
     }
 }
