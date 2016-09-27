9e3214e09082618c42b4c2c2dbea80897e89ed8f
Satyam Ghodasara <sghodas@gmail.com>
2016-09-26 22:08:06 -0400

    Added ForecastIOError to represent errors in the package

---
+++ Source/ForecastIOError.swift
@@ -0,0 +1,15 @@
+//
+//  ForecastIOError.swift
+//  ForecastIO
+//
+//  Created by Satyam Ghodasara on 9/26/16.
+//
+//
+
+import Foundation
+
+/// Used to represent errors in interacting with the Dark Sky API.
+public enum ForecastIOError: Error {
+    /// Error due to invalid JSON.
+    case invalidJSON(Data)
+}
