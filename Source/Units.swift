f44196cf62ae7e959cfd2659d859f52f3d910701
Satyam Ghodasara <sghodas@gmail.com>
2016-10-17 00:09:08 -0400

    Reverted to old style enum docs for Units

--- Source/Units.swift
+++ Source/Units.swift
@@ -9,16 +9,20 @@
 import Foundation
 
 /// Units in which the Dark Sky API data will be returned.
-///
-/// - si:   SI units. More details are available [in the Dark Sky API docs](https://darksky.net/dev/docs/forecast).
-/// - us:   US units and the default option.
-/// - ca:   Canadian units. Identical to `.SI` except `windSpeed` is in kilometers per hour.
-/// - uk:   UK units. Identical to `.SI` except `windSpeed` is in miles per hour, and `nearestStormDistance` and `visibility` are in miles.
-/// - auto: Automatically use the appropriate units based on the location for which you are requesting data.
 public enum Units: String {
+    
+    /// SI units. More details are available [in the Dark Sky API docs](https://darksky.net/dev/docs/forecast).
     case si = "si"
+    
+    /// US units and the default option.
     case us = "us"
+    
+    /// Canadian units. Identical to `.si` except `windSpeed` is in kilometers per hour.
     case ca = "ca"
+    
+    /// UK units. Identical to `.si` except `windSpeed` is in miles per hour, and `nearestStormDistance` and `visibility` are in miles.
     case uk = "uk2"
+    
+    /// Automatically use the appropriate units based on the location for which you are requesting data.
     case auto = "auto"
 }
