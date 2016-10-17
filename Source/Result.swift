169cebcb9f953dab3cebc6dcb50a30fc69711de3
Satyam Ghodasara <sghodas@gmail.com>
2016-10-17 00:07:45 -0400

    Reverted to old style enum docs for Result

--- Source/Result.swift
+++ Source/Result.swift
@@ -9,11 +9,12 @@
 import Foundation
 
 /// Represents whether a request was successful or encountered an error.
-///
-/// - success: The request and all post processing operations were successful resulting in the serialization of the provided associated value.
-/// - failure: The request encountered an error resulting in a failure. The associated values are the original data provided by the server as well as the error that caused the failure.
 public enum Result<Value> {
+    
+    /// The request and all post processing operations were successful.
     case success(Value, RequestMetadata)
+    
+    /// The request encountered an error resulting in a failure.
     case failure(Error)
     
     /// Returns the associated value if the result is a success, `nil` otherwise.
