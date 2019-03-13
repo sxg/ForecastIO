a180acce85a01efe2c3dbe928ffbcbd438c5acca
Satyam Ghodasara <sghodas@gmail.com>
2019-03-13 16:38:08 -0400

    Add more rigorous testing of Flag properties

--- Tests/FlagTests.swift
+++ Tests/FlagTests.swift
@@ -37,6 +37,15 @@ class FlagTests: XCTestCase {
         
         //  Then
         XCTAssertNotNil(flag)
+        XCTAssertNil(flag.darkSkyUnavailable)
+        XCTAssertEqual(flag.darkSkyStations, ["KLWX", "KDOX"])
+        XCTAssertNil(flag.dataPointStations)
+        XCTAssertEqual(flag.isdStations, ["724060-93721", "745944-93784", "997296-99999", "997302-99999", "999999-13725"])
+        XCTAssertEqual(flag.lampStations, ["KADW", "KAPG", "KBWI", "KCGS", "KDCA", "KDMH", "KDMW", "KFME", "KMTN", "KNAK"])
+        XCTAssertNil(flag.metarStations)
+        XCTAssertNil(flag.metnoLicense)
+        XCTAssertEqual(flag.sources, ["darksky", "lamp", "gfs", "cmc", "nam", "rap", "rtma", "sref", "fnmoc", "isd", "nwspa", "madis", "nearest-precip"])
+        XCTAssertEqual(flag.units, Units.us)
     }
     
 }
