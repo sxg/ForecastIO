96eb5ff250c3beda3322fba801f63fc05d1f309a
Satyam Ghodasara <sghodas@gmail.com>
2019-03-13 22:51:48 -0400

    Updated Results unit testing with new errors

--- Tests/ResultTests.swift
+++ Tests/ResultTests.swift
@@ -12,36 +12,64 @@ import XCTest
 class ResultTests: XCTestCase {
     
     func testSuccessfulResult() {
-        //  Given
+        // Given
         let forecastJSONPath = Bundle(for: type(of: self)).path(forResource: "forecast", ofType: "json")!
         let forecastJSONData = try! Data(contentsOf: URL(fileURLWithPath: forecastJSONPath))
-        let forecastJSON = try! JSONSerialization.jsonObject(with: forecastJSONData, options: .mutableContainers) as! NSDictionary
-        let forecast = Forecast(fromJSON: forecastJSON)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        let forecast = try! decoder.decode(Forecast.self, from: forecastJSONData)
         
         let httpHeaderFields = ["Cache-Control": "max-age=60", "X-Forecast-API-Calls": "12", "X-Response-Time": "123.45ms"]
         let requestMetadata = RequestMetadata(fromHTTPHeaderFields: httpHeaderFields)
         let result = Result.success(forecast, requestMetadata)
         
-        //  When
+        // When
         let (aForecast, aRequestMetadata) = result.value
         let anError = result.error
         
-        //  Then
+        // Then
         XCTAssertNotNil(aForecast)
         XCTAssertNotNil(aRequestMetadata)
         XCTAssertNil(anError)
     }
     
-    func testFailureResult() {
-        //  Given
-        let forecastIOError = ForecastIOError.invalidJSON(Data())
-        let result = Result<Forecast>.failure(forecastIOError)
+    func testFailureResultMissingData() {
+        // Given
+        let result = Result<Forecast>.failure(ForecastIOError.missingData)
         
-        //  When
+        // When
         let (aForecast, aRequestMetadata) = result.value
         let anError = result.error
         
-        //  Then
+        // Then
+        XCTAssertNil(aForecast)
+        XCTAssertNil(aRequestMetadata)
+        XCTAssertNotNil(anError)
+    }
+    
+    func testFailureResultMissingResponse() {
+        // Given
+        let result = Result<Forecast>.failure(ForecastIOError.missingResponse)
+        
+        // When
+        let (aForecast, aRequestMetadata) = result.value
+        let anError = result.error
+        
+        // Then
+        XCTAssertNil(aForecast)
+        XCTAssertNil(aRequestMetadata)
+        XCTAssertNotNil(anError)
+    }
+    
+    func testFailureResultUnexpectedError() {
+        // Given
+        let result = Result<Forecast>.failure(ForecastIOError.unexpectedError)
+        
+        // When
+        let (aForecast, aRequestMetadata) = result.value
+        let anError = result.error
+        
+        // Then
         XCTAssertNil(aForecast)
         XCTAssertNil(aRequestMetadata)
         XCTAssertNotNil(anError)
