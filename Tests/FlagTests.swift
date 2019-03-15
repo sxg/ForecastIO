4fe3f5ba04ff82a225188754889cf62d4d0dd65c
Satyam Ghodasara <sghodas@gmail.com>
2019-03-14 21:56:47 -0400

    Update flag.json and Flag unit tests

--- Tests/FlagTests.swift
+++ Tests/FlagTests.swift
@@ -38,13 +38,8 @@ class FlagTests: XCTestCase {
         // Then
         XCTAssertNotNil(flag)
         XCTAssertNil(flag.darkSkyUnavailable)
-        XCTAssertEqual(flag.darkSkyStations, ["KLWX", "KDOX"])
-        XCTAssertNil(flag.dataPointStations)
-        XCTAssertEqual(flag.isdStations, ["724060-93721", "745944-93784", "997296-99999", "997302-99999", "999999-13725"])
-        XCTAssertEqual(flag.lampStations, ["KADW", "KAPG", "KBWI", "KCGS", "KDCA", "KDMH", "KDMW", "KFME", "KMTN", "KNAK"])
-        XCTAssertNil(flag.metarStations)
-        XCTAssertNil(flag.metnoLicense)
-        XCTAssertEqual(flag.sources, ["darksky", "lamp", "gfs", "cmc", "nam", "rap", "rtma", "sref", "fnmoc", "isd", "nwspa", "madis", "nearest-precip"])
+        XCTAssertEqual(flag.nearestStation, 1.839)
+        XCTAssertEqual(flag.sources, ["nearest-precip", "nwspa", "cmc", "gfs", "hrrr", "icon", "isd", "madis", "nam", "sref", "darksky"])
         XCTAssertEqual(flag.units, Units.us)
     }
     
